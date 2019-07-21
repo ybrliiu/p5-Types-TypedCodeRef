081e31e498788c6585d5fa6da7922932e1c425b9
mp0liiu <raian@reeshome.org>
2019-07-21 20:20:25 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Types::AnonSub - It's new $module
+
+# SYNOPSIS
+
+    use Types::AnonSub;
+
+# DESCRIPTION
+
+Types::AnonSub is ...
+
+# LICENSE
+
+Copyright (C) mp0liiu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+mp0liiu <raian@reeshome.org>
---
+++ lib/Types/AnonSub.pm
@@ -0,0 +1,39 @@
+package Types::AnonSub;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Types::AnonSub - It's new $module
+
+=head1 SYNOPSIS
+
+    use Types::AnonSub;
+
+=head1 DESCRIPTION
+
+Types::AnonSub is ...
+
+=head1 LICENSE
+
+Copyright (C) ybrliiu.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+ybrliiu E<lt>raian@reeshome.orgE<gt>
+
+=cut
+
