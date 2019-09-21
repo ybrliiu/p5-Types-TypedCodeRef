470f98d9f8d9cec24412fc37045ca4d7e9ef2be3
mp0liiu <raian@reeshome.org>
2019-09-21 19:28:47 +0900

    fix readme

--- lib/Types/TypedCodeRef.pm
+++ lib/Types/TypedCodeRef.pm
@@ -192,7 +192,7 @@ Types::TypedCodeRef - Types for any typed anonymous subroutine.
     use Test2::V0;
     use Types::TypedCodeRef -types;
     use Types::Standard qw( Int Str );
-    use Sub::TypedAnon;
+    use Sub::Anon::Typed qw( anon );
     
     my $type = TypedCodeRef[ [Int, Int] => Int ];
     ok $type->check(anon [Int, Int] => Int, sub { $_[0] + $_[1] });
